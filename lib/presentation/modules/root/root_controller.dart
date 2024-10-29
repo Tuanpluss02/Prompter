1cab3d1a3e9484fea197544740de4ebc6d82615f
Tuanpluss02 <dongoctuan2002sp@gmail.com>
2024-10-30 01:07:54 +0700

    feat: Update VSCode settings for explicit code actions
    - Update the VSCode settings to use explicit code actions for organizing and sorting imports.
    - Add "editor.accessibilitySupport" setting and set it to "off".

--- lib/presentation/modules/root/root_controller.dart
+++ lib/presentation/modules/root/root_controller.dart
@@ -1,31 +1,27 @@
-import 'dart:async';
-
+import 'package:base/app/utils/log.dart';
 import 'package:base/base/base_controller.dart';
 import 'package:base/presentation/modules/home/home_screen.dart';
-import 'package:base/presentation/routes/app_pages.dart';
-import 'package:base/services/auth_service.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class RootController extends BaseController {
-  StreamSubscription? _authStateChangesSubscription;
-  final AuthService _authService = Get.find<AuthService>();
+  // StreamSubscription? _authStateChangesSubscription;
+  // final AuthService _authService = Get.find<AuthService>();
 
   @override
   void onInit() {
     super.onInit();
-    _authStateChangesSubscription = _authService.user.listen((User? user) {
-      if (user == null) {
-        Get.snackbar('Thông báo', "Đăng xuất thành công");
-        Get.offAllNamed(AppRoutes.login);
-      }
-    });
+    // _authStateChangesSubscription = _authService.user.listen((User? user) {
+    //   if (user == null) {
+    //     Get.snackbar('Thông báo', "Đăng xuất thành công");
+    //     Get.offAllNamed(AppRoutes.login);
+    //   }
+    // });
   }
 
   @override
   void onClose() {
-    _authStateChangesSubscription?.cancel();
+    // _authStateChangesSubscription?.cancel();
     super.onClose();
   }
 
@@ -45,6 +41,10 @@ class RootController extends BaseController {
   ];
 
   onNavItemTaped(int index) {
-    currentIndex.value = index;
+    Log.console('onNavItemTaped $index');
+    if (index == 2) {
+      return;
+    }
+    currentIndex.value = index >= 2 ? index - 1 : index;
   }
 }
--- lib/presentation/modules/root/root_screen.dart
+++ lib/presentation/modules/root/root_screen.dart
@@ -1,5 +1,6 @@
 import 'package:base/base/base_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:forui/forui.dart';
 import 'package:get/get.dart';
 
 import 'root_controller.dart';
@@ -12,6 +13,37 @@ class RootScreen extends BaseScreen<RootController> {
     return Obx(() => IndexedStack(
           index: controller.currentIndex.value,
           children: controller.screens,
+        ));
+  }
+
+  @override
+  Widget? buildBottomNavigationBar(BuildContext context) {
+    return Obx(() => FBottomNavigationBar(
+          index: controller.currentIndex.value,
+          onChange: controller.onNavItemTaped,
+          children: [
+            FBottomNavigationBarItem(
+              icon: FIcon(FAssets.icons.house),
+              label: const Text('Home'),
+            ),
+            FBottomNavigationBarItem(
+              icon: FIcon(FAssets.icons.layoutGrid),
+              label: const Text('Browse'),
+            ),
+            Container(
+              width: 20,
+              height: 20,
+              color: Colors.amber,
+            ),
+            FBottomNavigationBarItem(
+              icon: FIcon(FAssets.icons.libraryBig),
+              label: const Text('Library'),
+            ),
+            FBottomNavigationBarItem(
+              icon: FIcon(FAssets.icons.search),
+              label: const Text('Search'),
+            ),
+          ],
         ));
   }
 }
