d2bc2dac13299d55e89e53067c62b07f038aa1f7
tuan.dn <dongoctuan2002sp@gmail.com>
2024-11-09 19:36:19 +0700

    feat: Implement basic account screen UI
    Creates an initial UI for the account screen, including a profile avatar, user's name display, and a styled app bar with a title.

--- lib/presentation/modules/account/account_screen.dart
+++ lib/presentation/modules/account/account_screen.dart
@@ -1,5 +1,7 @@
+import 'package:base/app/constants/app_color.dart';
 import 'package:base/base/base_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import 'account_controller.dart';
 
@@ -7,7 +9,32 @@ class AccountScreen extends BaseScreen<AccountController> {
   const AccountScreen({super.key});
 
   @override
+  PreferredSizeWidget? buildAppBar(BuildContext context) {
+    return AppBar(
+      title: Text('Account',
+          style: GoogleFonts.manrope(
+            color: Colors.white,
+            fontSize: 28,
+            fontWeight: FontWeight.w600,
+          )),
+      centerTitle: true,
+      backgroundColor: AppColors.backgroundColor,
+    );
+  }
+
+  @override
   Widget buildScreen(BuildContext context) {
-    return Container();
+    return SingleChildScrollView(
+        child: Column(children: [
+      SizedBox(height: 20),
+      CircleAvatar(
+        radius: 50,
+        backgroundImage: AssetImage('assets/images/avatar.png'),
+      ),
+      SizedBox(height: 20),
+      Text(
+        'John Doe',
+      )
+    ]));
   }
 }
