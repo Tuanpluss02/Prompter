43298206b0085d703d573017f548cad5273ff7b1
tuan.dn <dongoctuan2002sp@gmail.com>
2024-11-13 00:19:46 +0700

    Refactor: Revamp Account Screen UI
    Redesigned the Account screen for a more visually appealing and informative user experience.  Replaced the static avatar and name with dynamic content fetched from a network source.  Introduced a background image and gradient overlay for enhanced aesthetics.  Improved layout structure for better content organization.

--- lib/presentation/modules/account/account_screen.dart
+++ lib/presentation/modules/account/account_screen.dart
@@ -1,7 +1,6 @@
-import 'package:base/app/constants/app_color.dart';
 import 'package:base/base/base_screen.dart';
+import 'package:base/presentation/widgets/global/app_image.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 import 'account_controller.dart';
 
@@ -9,32 +8,73 @@ class AccountScreen extends BaseScreen<AccountController> {
   const AccountScreen({super.key});
 
   @override
-  PreferredSizeWidget? buildAppBar(BuildContext context) {
-    return AppBar(
-      title: Text('Account',
-          style: GoogleFonts.manrope(
-            color: Colors.white,
-            fontSize: 28,
-            fontWeight: FontWeight.w600,
-          )),
-      centerTitle: true,
-      backgroundColor: AppColors.backgroundColor,
-    );
-  }
-
-  @override
   Widget buildScreen(BuildContext context) {
     return SingleChildScrollView(
-        child: Column(children: [
-      SizedBox(height: 20),
-      CircleAvatar(
-        radius: 50,
-        backgroundImage: AssetImage('assets/images/avatar.png'),
-      ),
-      SizedBox(height: 20),
-      Text(
-        'John Doe',
-      )
-    ]));
+        physics: BouncingScrollPhysics(),
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              SizedBox(
+                height: 230,
+                width: double.infinity,
+                child: Stack(
+                  clipBehavior: Clip.none,
+                  fit: StackFit.loose,
+                  children: [
+                    AppImage(
+                        fit: BoxFit.contain,
+                        image: NetworkImage(
+                            'https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg')),
+                    Container(
+                      decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                              begin: Alignment(0, 0.2),
+                              end: Alignment.bottomCenter,
+                              colors: [
+                            Colors.transparent,
+                            Colors.black.withOpacity(0.6),
+                          ])),
+                    )
+                  ],
+                ),
+              ),
+              Transform.translate(
+                  offset: Offset(0, -80), child: _buildMainContent())
+            ]));
+  }
+
+  _buildMainContent() {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              radius: 50,
+              backgroundImage: NetworkImage(
+                  'https://danviet.mediacdn.vn/296231569849192448/2024/6/13/son-tung-mtp-17182382517241228747767.jpg'),
+            ),
+            SizedBox(width: 20),
+            RichText(
+              text: TextSpan(
+                children: [
+                  TextSpan(
+                      text: 'Son Tung MTP\n',
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                  TextSpan(
+                      text: '@sontungmtp',
+                      style: TextStyle(fontSize: 16, color: Colors.grey)),
+                ],
+              ),
+            ),
+          ],
+        ),
+        SizedBox(height: 20),
+      ],
+    );
   }
 }
