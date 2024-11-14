22bc173f4ed99f97d9666b44b1027a8501da55b9
tuan.dn <dongoctuan2002sp@gmail.com>
2024-11-14 08:59:34 +0700

    feat: Setup AIChat feature
    Initial setup for the AIChat feature. This includes the controller, binding, and screen components, laying the groundwork for AI Chat functionality.  The screen implementation is still a placeholder.

---
+++ lib/presentation/modules/ai_chat/ai_chat_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:base/base/base_screen.dart';
+
+import 'ai_chat_controller.dart';
+
+class AiChatScreen extends BaseScreen<AiChatController> {
+  const AiChatScreen({super.key});
+
+
+  @override
+  Widget buildScreen(BuildContext context) {
+    // TODO: implement AiChatPage
+    throw UnimplementedError();
+  }
+}
