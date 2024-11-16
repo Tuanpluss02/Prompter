6f66380061edad14a1172621d1f9207f13182d0c
Tuanpluss02 <dongoctuan2002sp@gmail.com>
2024-11-17 01:14:00 +0700

    Updates Generative AI model URLs
    Replaces placeholder and outdated URLs with up-to-date links for model icons.

--- lib/app/constants/app_enums.dart
+++ lib/app/constants/app_enums.dart
@@ -1,13 +1,11 @@
-import 'package:base/app/constants/app_strings.dart';
-
 enum ModelType { textToText, textToImage }
 
 enum GenerativeAiModel {
-  midjourney('Jovie/Midjourney', 'Midjourney', 'https://upload.wikimedia.org/wikipedia/commons/2/24/Midjourney_Emblem.svg', ModelType.textToImage),
+  midjourney('Jovie/Midjourney', 'Midjourney', 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e6/Midjourney_Emblem.png/768px-Midjourney_Emblem.png', ModelType.textToImage),
   stableDiffusion(
       'stabilityai/stable-diffusion-3.5-large', 'Stable Diffusion', 'https://images.prismic.io/encord/Zeb6euXgT-BdbvJy_image-47-.png?auto=format%2Ccompress&fit=max', ModelType.textToImage),
   flux('black-forest-labs/FLUX.1-dev', 'FLUX', 'https://www.blackforestlabs.ai/wp-content/uploads/2024/07/logo-with-text_more-1024x970.png', ModelType.textToImage),
-  gemini('Gemini', 'Gemini', AppStrings.defaultNetworkAvatar, ModelType.textToText);
+  gemini('Gemini', 'Gemini', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThr7qrIazsvZwJuw-uZCtLzIjaAyVW_ZrlEQ&s', ModelType.textToText);
 
   final String modelId;
   final String displayName;
