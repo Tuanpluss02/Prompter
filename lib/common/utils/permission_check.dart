04a02fe376cc8d86257774f0274253afabdcb87a
Tuanpluss02 <dongoctuan2002sp@gmail.com>
2024-11-17 14:23:22 +0700

    Improves permission request dialogs
    Provides more context to the user when requesting permissions by including the specific permission name in the dialog message.
    
    Adds an extension on the `Permission` enum to get human-readable permission names.  This makes the dialog messages more informative and user-friendly.

--- lib/common/utils/permission_check.dart
+++ lib/common/utils/permission_check.dart
@@ -9,14 +9,14 @@ Future<bool> checkAndRequestPermission({required Permission permission}) async {
     case PermissionStatus.granted:
       return true;
     case PermissionStatus.denied:
-      bool isGranted = await _showRequestPermissionDialog();
+      bool isGranted = await _showRequestPermissionDialog(permission.name);
       if (isGranted) {
         PermissionStatus newStatus = await Permission.storage.request();
         return newStatus.isGranted;
       }
       break;
     case PermissionStatus.permanentlyDenied:
-      bool isGranted = await _showOpenSettingDialog();
+      bool isGranted = await _showOpenSettingDialog(permission.name);
       if (isGranted) {
         openAppSettings();
       }
@@ -27,10 +27,10 @@ Future<bool> checkAndRequestPermission({required Permission permission}) async {
   return false;
 }
 
-Future<bool> _showRequestPermissionDialog() async {
+Future<bool> _showRequestPermissionDialog(String permissionName) async {
   return Get.dialog<bool>(AppDialog(
     title: "Permission Required",
-    content: "This app needs storage permission to save images.",
+    content: "You need to grant $permissionName permission to continue.",
     primaryButtonText: "Allow",
     secondaryButtonText: "Deny",
     onPrimaryButtonTap: () => Get.back(result: true),
@@ -38,13 +38,31 @@ Future<bool> _showRequestPermissionDialog() async {
   )).then((value) => value ?? false);
 }
 
-Future<bool> _showOpenSettingDialog() async {
+Future<bool> _showOpenSettingDialog(String permissionName) async {
   return Get.dialog<bool>(AppDialog(
     title: "Permission Required",
-    content: "This app needs storage permission to save images. You can grant the permission from the app settings.",
+    content: "This app needs $permissionName permission to continue. You can grant the permission from the app settings.",
     primaryButtonText: "Allow",
     secondaryButtonText: "Deny",
     onPrimaryButtonTap: () => Get.back(result: true),
     onSecondaryButtonTap: () => Get.back(result: false),
   )).then((value) => value ?? false);
+}
+
+extension GetPermissionName on Permission {
+  String get name {
+    return switch (this) {
+      Permission.camera => "Camera",
+      Permission.storage => "Storage",
+      Permission.photos => "Photos",
+      Permission.mediaLibrary => "Media Library",
+      Permission.microphone => "Microphone",
+      Permission.speech => "Speech",
+      Permission.location => "Location",
+      Permission.notification => "Notification",
+      Permission.bluetooth => "Bluetooth",
+      Permission.contacts => "Contacts",
+      _ => ""
+    };
+  }
 }
