b439f79a65a820e335e946a601a393b1b14d0b30
Tuanpluss02 <dongoctuan2002sp@gmail.com>
2024-11-20 22:46:24 +0700

    Replaces AppImage with ExtendedImage
    Migrates from the custom `AppImage` widget to the `extended_image` package for enhanced image loading and error handling.  This improves image rendering performance and provides a fallback for failed image loads.

--- lib/presentation/modules/home/components/user_section.dart
+++ lib/presentation/modules/home/components/user_section.dart
@@ -3,7 +3,7 @@ import 'package:base/common/constants/app_strings.dart';
 import 'package:base/common/constants/app_text_styles.dart';
 import 'package:base/domain/data/entities/user_entity.dart';
 import 'package:base/presentation/shared/animated/animated_scale_button.dart';
-import 'package:base/presentation/shared/global/app_image.dart';
+import 'package:extended_image/extended_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:timeago/timeago.dart' as timeago;
@@ -41,9 +41,10 @@ class UserSection extends StatelessWidget {
             ),
           ),
           child: ClipOval(
-              child: AppImage(
-            image: NetworkImage(user.profileImage ?? AppStrings.defaultNetworkAvatar),
+              child: Image(
+            image: ExtendedNetworkImageProvider(user.profileImage ?? AppStrings.defaultNetworkAvatar),
             fit: BoxFit.cover,
+            errorBuilder: (context, error, stackTrace) => SvgPicture.asset(SvgPath.icAttention),
           )),
         ),
         SizedBox(width: 20),
