e22b89b4315ef8bee2635a7954d9fadf45d4774e
Tuanpluss02 <dongoctuan2002sp@gmail.com>
2024-12-17 20:38:32 +0700

    Renames 'Gemini' to 'Prompter Assistant' in app_enums.dart for clarity

--- lib/common/constants/app_enums.dart
+++ lib/common/constants/app_enums.dart
@@ -5,7 +5,7 @@ enum GenerativeAiModel {
   stableDiffusion(
       'stabilityai/stable-diffusion-3.5-large', 'Stable Diffusion', 'https://images.prismic.io/encord/Zeb6euXgT-BdbvJy_image-47-.png?auto=format%2Ccompress&fit=max', ModelType.textToImage),
   flux('black-forest-labs/FLUX.1-dev', 'FLUX', 'https://tjzk.replicate.delivery/models_organizations_avatar/01ed70be-0d47-4a4a-85fb-32c02cdd4ab5/bfl.png', ModelType.textToImage),
-  gemini('Gemini', 'Gemini', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThr7qrIazsvZwJuw-uZCtLzIjaAyVW_ZrlEQ&s', ModelType.textToText);
+  gemini('Gemini', 'Prompter Assistant', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThr7qrIazsvZwJuw-uZCtLzIjaAyVW_ZrlEQ&s', ModelType.textToText);
 
   final String modelId;
   final String displayName;
